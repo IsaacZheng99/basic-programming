8f61cfad80c631a7d30e5c3a00fe4f3dc08e5a41
IsaacZheng99 <isaaczheng.cs@gmail.com>
2025-08-04 11:42:09 +0800

    2-Code

--- algorithm/programmer_carl/Code/2-Linked_List/LinkedList/LinkedList/src/Main.cpp
+++ algorithm/programmer_carl/Code/2-Linked_List/LinkedList/LinkedList/src/Main.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include "142DetectCycle.h"
-
-
 int main()
 {
-	ListNode* head = new ListNode(1);
-	head->next = head;
-
-	Solution s;
-	ListNode* result = s.detectCycle(head);
-	std::cout << result->val << std::endl;
-
-
-
-
-
 
 }
